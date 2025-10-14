5b2b966b385b0d72ba4127e1de9def81c1bbcc3d
Bernhard Fischer-Wasels <Bernhard@fischer-wasels.de>
2025-10-14 17:15:43 +0200

    Update VC_BERICHTE_PDF_DDL.sql
    updated 14.10.25

--- VC_BERICHTE_PDF_DDL.sql
+++ VC_BERICHTE_PDF_DDL.sql
@@ -29,11 +29,12 @@ ALTER TRIGGER "BI_VC_BERICHTE_PDF" ENABLE;
    CREATE SEQUENCE  "VC_BERICHTE_PDF_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
 
 
-create or replace TRIGGER "A224048"."BI_VC_BERICHTE_PDF" 
+create or replace TRIGGER "BI_VC_BERICHTE_PDF" 
   before insert on "VC_BERICHTE_PDF"               
   for each row  
 begin   
   if :NEW."ID" is null then 
     select "VC_BERICHTE_PDF_SEQ".nextval into :NEW."ID" from sys.dual; 
   end if; 
+
 end; 
